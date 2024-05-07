c09e6b0ea556dcba6efed3cc7eae7eb3d6388b64
Bhargavibhatia215 <bhatiabhargavi215@gmail.com>
2024-05-07 19:58:23 +0530

    application for docker

---
+++ src/main/java/com/example/dockerDemo/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.dockerDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/welcome")
+    public String printHelloWorld(){
+        return  "Hello World";
+    }
+}
